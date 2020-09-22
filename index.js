eaed0bb6fab17cfb339dc5c239ce330fbdd774fc
Jacob Harris <jacob-harris@lambdastudents.com>
2020-09-21 22:03:25 -0400

    Finished tasks 1-3

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
-
-    /*code here*/
-
+function is31Flavors(flavorArray){
+    if (flavorArray.length === 31) {
+        return true;
+    } else return false;
 }
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -57,10 +57,9 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
-
-    /*code here*/
-
+function addFlavor(array, newFlavor){
+    array.unshift(newFlavor);
+    return array;
 }
 
 
@@ -74,10 +73,9 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
-
+function removeLastFlavor(array){
+    array.pop()
+    return array;
 }
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
